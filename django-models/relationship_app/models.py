e3a8f4f9a159f5e4a7ca08875205f75da568d554
Foster <kdwomoh0269@gmail.com>
2024-08-17 22:12:22 +0000

    django-models

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -29,19 +29,19 @@ class Librarian(models.Model):
 
 # relationship_app/models.py
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Admin
 
-class UserProfile(models.Model):
+class AdminProfile(models.Model):
 
-    USER_ROLES = (
+    Admin_role= (
     ('Admin', 'Admin'),
     ('Member', 'Member'),
     ('Admin'),
     ('Librarian', 'Librarian'),
     ('Member'),
     )
-    USER = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=50, choices=USER_ROLES, default='Member')
+    Admin = models.OneToOneField(UserAdmin, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=Admin_role, default='Member')
 
     def __str__(self):
         return f'{self.user.username} - {self.role}'
