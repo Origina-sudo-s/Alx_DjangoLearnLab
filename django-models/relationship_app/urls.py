19cc81233e1c4440f7c99427b2fe9928495a7f77
Foster <kdwomoh0269@gmail.com>
2024-08-17 21:58:10 +0000

    django-models

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,3 +6,13 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+
+# relationship_app
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    # other URLs
+]
