4d977812533b3b7949f84dc698b7426111eb6ddf
Foster <kdwomoh0269@gmail.com>
2024-08-17 23:01:01 +0000

    django-models

--- django-models/admin_view.py
+++ django-models/admin_view.py
@@ -53,3 +53,25 @@ def admin_required(view_func):
             else:
                 raise PermissionDenied
         return _wrapped_view
+
+
+
+# relationship_app/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+
+    USER_ROLES= (
+    ('Admin', 'Admin'),
+    ('Member', 'Member'),
+    ('Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member'),
+    )
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=USER_ROLES, default='Member')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
+        
