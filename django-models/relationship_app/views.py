fb0af2564bda90c37c71923ab5238c70b164b636
Foster <kdwomoh0269@gmail.com>
2024-08-17 21:37:25 +0000

    django-models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -17,6 +17,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+# relationship_app/views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .decorators import admin_required
+
+@login_required
+@admin_required
+def admin_view(request):
+    # Your view logic here
+    return render(request, 'relationship_app/admin_view.html')
 
 
 # relationship_app/views.py
