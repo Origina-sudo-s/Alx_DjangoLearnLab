41b4518f6143ae36d5f86e09ffb4cd00f34c5a52
Foster <kdwomoh0269@gmail.com>
2024-08-17 22:52:53 +0000

    django-models

--- django-models/admin_view.py
+++ django-models/admin_view.py
@@ -1,23 +1,14 @@
+# relationship_app/views.py
+
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
+from .decorators import admin_required
 
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
-
-@user_passes_test(is_admin)
+@login_required
+@admin_required
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    # Your view logic here
+    return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(is_librarian)
-def libraries_view(request):
-    return render(request, 'librarian_view.html')
 
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_view.html')
+
