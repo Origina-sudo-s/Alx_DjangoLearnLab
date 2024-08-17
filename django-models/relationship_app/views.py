84b8948705d8f899be7d6eb33fe7cac13483a7cf
Foster <kdwomoh0269@gmail.com>
2024-08-17 00:02:43 +0000

    django-models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -15,5 +15,5 @@ from relationship_app.models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
