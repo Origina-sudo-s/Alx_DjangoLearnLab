62167412541f59ee12a1ef40d19b14b75279aab4
Foster <kdwomoh0269@gmail.com>
2024-08-18 23:26:34 +0000

    django-models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -97,3 +97,35 @@ def edit_book(request, pk):
             book.delete()
             return redirect('book_list')
         return render(request, 'relationship_app/delete_book.html', {'book': book})
+
+
+# relationship_app/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import  UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
+
+# relationship_app/views.py
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        is form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return render(request, 'relationship_app/login.html', {'form': form})
+def user_login(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
