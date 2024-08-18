a3a30513ff4bdc861ff083c9da0fc3ec87499504
Foster <kdwomoh0269@gmail.com>
2024-08-18 23:37:08 +0000

    django-models

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -30,3 +30,13 @@ urlpatterns = [
     
 ]
 
+# relationship_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.user_register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('login/', views.user_logout, name='logout'),
+]
+
