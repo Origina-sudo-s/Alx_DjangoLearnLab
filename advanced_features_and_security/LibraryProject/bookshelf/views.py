8bb9a4f34dfc3266e0e1db1e98aea741d5043693
Foster <kdwomoh0269@gmail.com>
2024-08-20 23:58:05 +0000

    advanced_features_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Article
+from .models import Book
 
 # Create your views here.
 @permission_required('app_name.can_view', raise_exception=True)
-def article_list(request):
-    articles = Article.objects.all()
-    return render(request, 'article_list.html', {'article': articles})
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
 
 @permission_required('app_name.can_edit', raise_exception=True)
-def edit_article(request, pk):
-    article = get_object_or_404(Article, pk=pk)
+def edit_book(request, pk):
+    book = get_object_or_404(book, pk=pk)
     if request.method == 'POST':
         #Process from submission
         pass
-    return render(request, 'edit_article.html', {'article': article})
+    return render(request, 'edit_book.html', {'books': books})
